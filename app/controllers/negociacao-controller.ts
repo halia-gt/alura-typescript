1a138b8071251d8aa017a94e8f79472dc0c85bbf
Nathália <nathalia.gramst@gmail.com>
2022-07-11 17:02:33 -0300

    Lista de negociações

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,20 +1,21 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
-
     }
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
     }
 
@@ -29,7 +30,7 @@ export class NegociacaoController {
     limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
-        this.inputValue.value = '';
+        this.inputValor.value = '';
         this.inputData.focus();
     }
 }
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,13 +1,15 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 export class NegociacaoController {
     constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
     }
     criaNegociacao() {
@@ -18,5 +20,9 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
